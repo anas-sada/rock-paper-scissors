4080d3fe19e5151c370e33328ada5791598a4700
anas-sada <144807992+anas-sada@users.noreply.github.com>
2026-05-31 16:47:49 +0300

    Added random computer guess and the human guess functions

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
+
+
 console.log("hello world");
+
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice(){
+    let random = Math.random();
+
+    if (random < 1/3){
+        return "rock";
+    }
+    else if (random < ((1/3)*2)){
+        return "paper";
+    }
+    else {
+        return "scissors";
+    }
+
+}
+
+function getHumanChoice(){
+    let guess = prompt("Please enter your guess ?","rock, paper or scissors");
+    return guess; 
+}
+
+
+
+console.log(getComputerChoice() + ' ' +  getHumanChoice());
